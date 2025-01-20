e21c2e9f11c192b360f787822ee9248f53419ec7
Alonso Ramos <alonso24005@hotmail.com>
2025-01-20 08:42:52 -0500

    refactor: actualiza rutas para usar SoporteController y agrega rutas para DatosController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
-use App\Http\Controllers\MantenimientoController;
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\SoporteController;
+use App\Http\Controllers\DatosController;
 
-Route::get('/', [MantenimientoController::class, 'index']);
-Route::post('/guardar', [MantenimientoController::class, 'store'])->name('guardar');
+Route::get('/', [SoporteController::class, 'index']);
+Route::post('/guardar', [SoporteController::class, 'store'])->name('guardar');
+
+Route::get('/datos', [DatosController::class, 'index'])->name('datos.index');
+Route::get('/datos/{id}/edit', [DatosController::class, 'edit'])->name('datos.edit');
+Route::put('/datos/{id}', [DatosController::class, 'update'])->name('datos.update');
