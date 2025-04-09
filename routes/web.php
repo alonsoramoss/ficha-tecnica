858e613bf5b72db7eb21e3ed501d3237e1b77f1f
Alonso Ramos <alonso24005@hotmail.com>
2025-04-09 09:01:35 -0500

    feat: agrega ruta para buscar datos

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,6 @@ Route::get('/', [SoporteController::class, 'index']);
 Route::post('/guardar', [SoporteController::class, 'store'])->name('guardar');
 
 Route::get('/datos', [DatosController::class, 'index'])->name('datos.index');
+Route::get('/datos/buscar', [DatosController::class, 'buscar'])->name('datos.buscar');
 Route::get('/datos/{id}/edit', [DatosController::class, 'edit'])->name('datos.edit');
 Route::put('/datos/{id}', [DatosController::class, 'update'])->name('datos.update');
