d621d02171d2678cc9118f937e824573ec79d682
Alonso Ramos <alonso24005@hotmail.com>
2025-09-23 18:45:18 -0500

    build(vite): agrega nuevos archivos CSS y JS a la configuración

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/css/error.css",
+                "resources/js/pages/soporte.js",
+                "resources/js/pages/datos.js",
+                "resources/js/pages/edit_datos.js",
+            ],
             refresh: true,
         }),
     ],
