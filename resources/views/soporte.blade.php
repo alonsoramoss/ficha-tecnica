14e6d0edc10a81f63228d12436179e31663e1490
Alonso Ramos <alonso24005@hotmail.com>
2025-10-04 18:29:12 -0500

    style(soporte): mejora maquetado y estructura responsive de la vista

--- resources/views/soporte.blade.php
+++ resources/views/soporte.blade.php
@@ -4,11 +4,11 @@
 
 @section("content")
     <div class="container">
-        <form id="formFicha" name="formFicha" action="{{ route("guardar") }}" method="POST" class="p-4 p-sm-5 border shadow-lg">
+        <form id="formFicha" name="formFicha" action="{{ route("guardar") }}" method="POST" class="p-4 p-md-5 border shadow-lg">
             @csrf
             <div class="row mb-4">
-                <div class="col-12 col-md-4 d-flex justify-content-center">
-                    <img src="{{ asset("img/municipalidad-pisco.png") }}" alt="Municipalidad de Pisco" class="img-fluid mb-4 mb-sm-5" style="width: 19rem;">
+                <div class="d-flex justify-content-center col-12 col-md-4 mb-3">
+                    <img src="{{ asset("img/municipalidad-pisco.png") }}" alt="Municipalidad de Pisco" class="img-fluid" style="width: 19rem;">
                 </div>
                 <div class="col-12 col-md-8">
                     <label for="nomFicha" class="form-label fw-semibold">FICHA TÉCNICA</label>
@@ -22,13 +22,13 @@
                 </div>
             </div>
 
-            <h5 class="fw-semibold mb-3 text-center text-sm-start">INFORMACIÓN GENERAL</h5>
-            <div class="row mb-3">
-                <div class="col">
+            <h5 class="fw-semibold mb-3 text-center text-md-start">INFORMACIÓN GENERAL</h5>
+            <div class="row g-3 mb-3">
+                <div class="col-12 col-md">
                     <label for="unidOrganica" class="form-label fw-semibold">UNIDAD ORGÁNICA</label>
                     <input type="text" id="unidOrganica" name="unidOrganica" class="form-control" required>
                 </div>
-                <div class="col">
+                <div class="col-12 col-md">
                     <label for="fecha" class="form-label fw-semibold">FECHA</label>
                     <input type="date" id="fecha" name="fecha" class="form-control" max="{{ date('Y-m-d') }}" required>
                 </div>
@@ -37,60 +37,60 @@
                 <label for="encargado" class="form-label fw-semibold">ENCARGADO O RESPONSABLE</label>
                 <input type="text" id="encargado" name="encargado" class="form-control" required>
             </div>
-            <div class="row mb-3">
-                <div class="col">
+            <div class="row g-3 mb-3">
+                <div class="col-12 col-md">
                     <label for="cargo" class="form-label fw-semibold">CARGO</label>
                     <input type="text" id="cargo" name="cargo" class="form-control" required>
                 </div>
-                <div class="col">
+                <div class="col-12 col-md">
                     <label for="dni" class="form-label fw-semibold">DNI</label>
                     <input type="text" id="dni" name="dni" class="form-control" inputmode="numeric" required>
                 </div>
             </div>
             <div class="mb-3">
                 <label for="modalidadLab" class="form-label fw-semibold">MODALIDAD LABORAL</label>
-                <div class="row">
+                <div class="row g-3">
                     <div class="col-12 col-md-6">
-                        <label class="text-decoration-underline mb-1">Empleado</label> <br>
-                        <div class="form-check form-check-inline">
+                        <label class="text-decoration-underline mb-2">Empleado</label> <br>
+                        <div class="form-check d-block d-sm-inline-block me-3">
                             <input type="radio" id="empleadoPermanente" name="modalidadLab" value="Empleado Permanente" class="form-check-input" required>
                             <label for="empleadoPermanente" class="form-check-label">Permanente</label>
                         </div>
-                        <div class="form-check form-check-inline">
+                        <div class="form-check d-block d-sm-inline-block me-3">
                             <input type="radio" id="empleadoContratado" name="modalidadLab" value="Empleado Contratado" class="form-check-input" required>
                             <label for="empleadoContratado" class="form-check-label">Contratado</label>
                         </div>
-                        <div class="form-check form-check-inline">
+                        <div class="form-check d-block d-sm-inline-block me-3">
                             <input type="radio" id="empleadoCAS" name="modalidadLab" value="Empleado CAS" class="form-check-input" required>
                             <label for="empleadoCAS" class="form-check-label">CAS</label>
                         </div>
                     </div>
                     <div class="col-12 col-md-6">
-                        <label class="text-decoration-underline mb-1">Obrero</label> <br>
-                        <div class="form-check form-check-inline">
+                        <label class="text-decoration-underline mb-2">Obrero</label> <br>
+                        <div class="form-check d-block d-sm-inline-block me-3">
                             <input type="radio" id="obreroPermanente" name="modalidadLab" value="Obrero Permanente" class="form-check-input" required>
                             <label for="obreroPermanente" class="form-check-label">Permanente</label>
                         </div>
-                        <div class="form-check form-check-inline">
+                        <div class="form-check d-block d-sm-inline-block me-3">
                             <input type="radio" id="obreroContratado" name="modalidadLab" value="Obrero Contratado" class="form-check-input" required>
                             <label for="obreroContratado" class="form-check-label">Contratado</label>
                         </div>
-                        <div class="form-check form-check-inline">
+                        <div class="form-check d-block d-sm-inline-block me-3">
                             <input type="radio" id="obreroCAS" name="modalidadLab" value="Obrero CAS" class="form-check-input" required>
                             <label for="obreroCAS" class="form-check-label">CAS</label>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="mb-3">
+            <div class="mb-4">
                 <label for="nomTecnico" class="form-label fw-semibold">NOMBRE DEL TÉCNICO</label>
                 <input type="text" id="nomTecnico" name="nomTecnico" class="form-control" required>
             </div>
 
-            <h5 class="fw-semibold mb-3 text-center text-sm-start">CONSTANCIA DE ATENCIÓN DEL SERVICIO DE SOPORTE TÉCNICO</h5>
+            <h5 class="fw-semibold text-center text-md-start">CONSTANCIA DE ATENCIÓN DEL SERVICIO DE SOPORTE TÉCNICO</h5>
             <p class="text-ficha">Consta por el presente, haber realizado el Servicio Técnico de lo siguiente:</p>
             <div class="row">
-                <div class="col-12 col-md-6 mb-3">
+                <div class="col-12 col-lg-6">
                     <label class="form-label fw-semibold">HARDWARE</label>
                     <div class="input-group mb-3">
                         <div class="input-group-text d-flex align-items-center justify-content-between w-50">
@@ -150,7 +150,7 @@
                     </div>
                 </div>
 
-                <div id="sistemas" name="sistemas" class="col mb-3">
+                <div class="col-12 col-lg-6">
                     <label for="sistemas" class="form-label fw-semibold">SISTEMAS Y/O APLICATIVOS</label>
                     <div class="input-group mb-3">
                         <div class="input-group-text d-flex align-items-center justify-content-between w-50">
@@ -212,7 +212,7 @@
             </div>
             
             <div class="row">
-                <div id="software" name="software" class="col">
+                <div class="col-12 col-lg-6">
                     <label for="software" class="form-label fw-semibold">SOFTWARE</label>
                     <div class="input-group mb-3">
                         <div class="input-group-text d-flex align-items-center justify-content-between w-50">
@@ -265,7 +265,7 @@
                     </div>
                 </div>
                 
-                <div id="redes" name="redes" class="col">
+                <div class="col-12 col-lg-6">
                     <label for="redes" class="form-label fw-semibold">REDES</label>
                     <div class="input-group mb-3">
                         <div class="input-group-text d-flex align-items-center justify-content-between w-50">
@@ -311,11 +311,11 @@
                     </div>
                 </div>
             </div>
-            <div class="mb-4 mb-sm-5">
+            <div class="mt-1 mb-4 mb-md-5">
                 <label for="observacion" class="form-label fw-semibold">OBSERVACIÓN</label>
                 <textarea id="observacion" name="observacion" class="form-control" rows="3" required></textarea>
             </div>
-            <div class="d-grid mx-auto col-8 col-md-6 col-lg-4">
+            <div class="d-grid mx-auto col-6 col-md-5 col-lg-3">
                 <button type="submit" class="btn btn-primary btn-lg fw-semibold">Guardar</button>  
             </div>
         </form>
