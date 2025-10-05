f303a0025e7734d269f4b9cc7bfe0de06ee61826
Alonso Ramos <alonso24005@hotmail.com>
2025-10-05 16:31:17 -0500

    feat: agrega alerta al salir con cambios sin guardar

---
+++ resources/js/components/alertaSalidaEdit.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("formFicha");
+    if (!form) return;
+
+    let dataInicial = {};
+    let enviando = false;
+
+    function guardarEstadoInicial() {
+        dataInicial = {};
+        form.querySelectorAll("input, textarea, select").forEach(el => {
+            if (el.type === "checkbox" || el.type === "radio") {
+                dataInicial[el.name + el.value] = el.checked;
+            } else {
+                dataInicial[el.name] = el.value;
+            }
+        });
+    }
+    guardarEstadoInicial();
+
+    function formModificado() {
+        let modificado = false;
+        form.querySelectorAll("input, textarea, select").forEach(el => {
+            if (el.type === "checkbox" || el.type === "radio") {
+                if (dataInicial[el.name + el.value] !== el.checked) modificado = true;
+            } else {
+                if (dataInicial[el.name] !== el.value) modificado = true;
+            }
+        });
+        return modificado;
+    }
+
+    window.addEventListener("beforeunload", function (e) {
+        if (!enviando && formModificado()) {
+            e.preventDefault();
+            e.returnValue = '';
+        }
+    });
+
+    form.addEventListener("submit", function () {
+        enviando = true;
+        setTimeout(() => { guardarEstadoInicial()}, 200);
+    });
+});
